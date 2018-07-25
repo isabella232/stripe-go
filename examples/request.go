ee6ba8fb9a0934c6242af6d11c74fcc18b3c9d4a
Isaac Snow <isaacjsnow@gmail.com>
2018-07-25 13:58:22 -0700

    add params variable

--- examples/request.go
+++ examples/request.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/sourcegraph/stripe-go/client"
+import (
+	stripe "github.com/sourcegraph/stripe-go"
+	"github.com/sourcegraph/stripe-go/client"
+)
 
 func simpleRequestExample() {
+	var params *stripe.ChargeParams
+
 	sc := &client.API{}
 	sc.Init("sk_test_BQokikJOvBiI2HlWgH4olfQ2", nil)
 	sc.Charges.Get("ch_1CqCgQ2eZvKYlo2CzBnALR20", params)
